36acd91d31dccb293258df07cbd3020f3dc442c0
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-06-26 15:11:18 -0400

    adds head() returning first element of an array

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  let isEqual = actual === expected;
+  const status = isEqual ? "👍 Assertion Passed: " : "👎 Assertion Failed: ";
+  const comparator = isEqual ? " === " : " !== ";
+
+  console.log(`${status}${actual}${comparator}${expected}`);
+};
+
+
+const head = function(array) {
+  
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
