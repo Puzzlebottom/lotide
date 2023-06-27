ae5f7a6e4e323c3fd081ec4a08b2e063c9163048
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-06-27 14:18:54 -0400

    minor refactor of tests for style points

--- without.js
+++ without.js
@@ -31,10 +31,12 @@ const words = ["abecedarian", "brobdingnagian", "cassandraic"];
 const wordsWithout = without(words, ["brobdingnagian"]);
 const numbersWithout = without([1, 2, 3], [3]);
 const numberStringsWithout = without(["1", "2", "3"], [1, 2, "3"]);
+const emptySource = without([], ["deconstructivism"]);
+const emptyItemsToRemove = without(["elephantiasis"], []);
 
 assertArraysEqual(wordsWithout, ["abecedarian", "cassandraic"]);
 assertArraysEqual(words, ["abecedarian", "brobdingnagian", "cassandraic"]);
 assertArraysEqual(numbersWithout, [1, 2]);
 assertArraysEqual(numberStringsWithout, ["1", "2"]);
-assertArraysEqual(without([], ["deconstructivism"]), []);
-assertArraysEqual(without(["elephantiasis"], []), ["elephantiasis"]);
+assertArraysEqual(emptySource, []);
+assertArraysEqual(emptyItemsToRemove, ["elephantiasis"]);
