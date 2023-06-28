37f1fd9d440817a119e992932392a04f93c75ffb
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-06-28 13:08:46 -0400

    adds basic functionality of countOnly(), with some failing tests

---
+++ countOnly.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  let isEqual = actual === expected;
+  const status = isEqual ? "👍 Assertion Passed: " : "👎 Assertion Failed: ";
+  const comparator = isEqual ? " === " : " !== ";
+
+  console.log(`${status}${actual}${comparator}${expected}`);
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  let result = {};
+  const keys = Object.keys(itemsToCount);
+  for (let item of allItems) {
+    if (keys.includes(item)) {
+      result[item] = result[item] ? result[item]++ : 1;
+    }
+  }
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
