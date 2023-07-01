b041c3b2ef869facaa80dac67929a15c8bb7786e
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-01 15:13:34 -0400

    adds recursive checks for object equality and passing assertions

--- eqObjects.js
+++ eqObjects.js
@@ -1,42 +1,89 @@
 const assertEqual = require("./assertEqual");
 const eqArrays = require('./eqArrays');
 
+const XOR = (a, b) => (a || b) && !(a && b);
+
 const eqObjects = (object1, object2) => {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
 
-  let result = true;
-
   if (keys1.length !== keys2.length) return false;
+
+  let result = true;
 
   for (let key of keys1) {
     let value1 = object1[key];
     let value2 = object2[key];
 
+    //undefined value case
+    if (value1 === undefined) throw new Error("Cannot compare objects containing values of undefined");
+
+    //primitive case
+    if (value1 === value2) continue;
+
+    //missing key case ()
     if (!value2) return false;
 
+    //only one array case;
+    if (XOR(Array.isArray(value1), Array.isArray(value2))) return false;
+
+    //two array case
     if (Array.isArray(value1) && Array.isArray(value2)) {
       result = eqArrays(value1, value2);
-    } else {
-      result = value1 === value2;
     }
+
+    //recursive case
+    result = eqObjects(value1, value2);
   }
   return result;
 };
 
 module.exports = eqObjects;
 
-// const cat1 = { name: "Breakfast", attack: "floppiness" };
-// const cat2 = { name: "Vada", attack: "seismic slam" };
-// assertEqual(eqObjects(cat1, cat2), false);
+const cat1 = { name: "Breakfast", attack: "floppiness" };
+const cat2 = { name: "Vada", attack: "seismic slam" };
+assertEqual(eqObjects(cat1, cat2), false);
 
-// const snowflake1 = { composition: "ice crystals", points: 6 };
-// const snowflake2 = { composition: "ice crystals", points: 6 };
-// assertEqual(eqObjects(snowflake1, snowflake2), true);
+const snowflake1 = { composition: "ice crystals", points: 6 };
+const snowflake2 = { composition: "ice crystals", points: 6 };
+assertEqual(eqObjects(snowflake1, snowflake2), true);
 
-// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-// assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
-// const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-// assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+
+const nestedObject1 = {
+  key1: {
+    keyA: ["A"],
+    keyB: "B"
+  },
+  key2: {
+    keyA: "A",
+    keyB: "B"
+  }
+};
+
+const nestedObject2 = {
+  key1: {
+    keyA: ["A"],
+    keyB: "B"
+  },
+  key2: {
+    keyA: "A",
+    keyB: "B"
+  }
+};
+
+const nestedObject3 = {
+  key1: [],
+  key2: {
+    keyA: "A",
+    keyB: "B"
+  }
+};
+
+assertEqual(eqObjects(nestedObject1, nestedObject2), true);
+assertEqual(eqObjects(nestedObject1, nestedObject3), false);
