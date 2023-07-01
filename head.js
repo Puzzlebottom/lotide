c65b1fd25ecb246d573af069149469eed327e8a6
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-01 12:08:52 -0400

    adds testing for head() and cleans up comments and imports

--- head.js
+++ head.js
@@ -1,11 +1,7 @@
-const assertEqual = require("./assertEqual");
-
 const head = function(array) {
+  if (!array) throw new Error("head() requires an array to be passed as an argument");
+  if (arguments.length > 1) throw new Error("too many arguments");
   return array[0];
 };
 
 module.exports = head;
-
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), undefined);
---
+++ test/headTest.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const head = require('../head');
+
+describe('head', function() {
+
+  it('should throw an error when passed no input', function() {
+    assert.throws(head, "head() requires an array to be passed as an argument");
+  });
+  it('should throw an error when passed more than one argument', function() {
+    assert.throws(() => head([1], [2]), "too many arguments");
+  });
+  it('should return undefined when passed an empty array', function() {
+    const input = [];
+    assert.equal(head(input), undefined);
+  });
+  it('should return the first element of an array', function() {
+    const input = [1, 2, 3];
+    const expectedOutput = 1;
+    assert.equal(head(input), expectedOutput);
+  });
+});
