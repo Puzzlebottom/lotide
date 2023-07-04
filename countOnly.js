d074b94663b180123ad6dc55d8fa320eff6132d5
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-03 21:19:00 -0400

    adds passing tests for countOnly()

--- countOnly.js
+++ countOnly.js
@@ -1,6 +1,8 @@
-const assertEqual = require("./assertEqual");
+const countOnly = function(allItems, itemsToCount) {
+  if (arguments.length < 2) throw new Error("Not enough arguments! countOnly() requires 2 arguments: an array of items, and an object whose keys will be counted");
+  if (arguments.length > 2) throw new Error("Too many arguments! countOnly() requires 2 arguments: an array of items, and an object whose keys will be counted");
+  if (!Array.isArray(allItems) || typeof itemsToCount !== "object") throw new Error("Wrong argument types! countOnly() requires 2 arguments: an array of items, and an object whose keys will be counted");
 
-const countOnly = (allItems, itemsToCount) => {
   let result = {};
   const keys = Object.keys(itemsToCount);
   for (let item of allItems) {
