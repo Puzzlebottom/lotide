4d56b494324315277f474da510e2483290d3429b
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-03 20:55:33 -0400

    adds passing tests for countLetters()

--- countLetters.js
+++ countLetters.js
@@ -1,8 +1,12 @@
 const assertEqual = require("./assertEqual");
 
-const countLetters = (string) => {
-  if (typeof string !== "string") return console.log(`These ${typeof string}s ain't no good fer countin'! How's about you gimme a string instead?`);
+const countLetters = function(string) {
+  if (arguments.length === 0) throw new Error("Not enough arguments! countLetters() takes one argument, a string whose letters are to be counted");
+  if (arguments.length > 1) throw new Error("Too many arguments! countLetters() takes one argument, a string whose letters are to be counted");
+  if (typeof string !== "string") throw new Error("Wrong argument type! countLetters() takes one argument, a string whose letters are to be counted");
+
   let result = {};
+  if (string === "") return result;
   for (let char of string.split(" ").join("").toLowerCase()) {
     result[char] = result[char] ? result[char] += 1 : 1;
   }
---
+++ test/countLettersTest.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const countLetters = require('../countLetters');
+const assertEqual = require('../assertEqual');
+
+describe('countLetters when it receives invalid output', function() {
+  it('should throw an error if it receives the wrong number of arguments', function() {
+
+    assert.throws(() => countLetters("a", "b"), "Too many arguments! countLetters() takes one argument, a string whose letters are to be counted");
+    assert.throws(() => countLetters(), "Not enough arguments! countLetters() takes one argument, a string whose letters are to be counted");
+  });
+  it('should throw an error if it receives anything but a string as an argument', function() {
+
+    assert.throws(() => countLetters({}), "Wrong argument type! countLetters() takes one argument, a string whose letters are to be counted");
+  });
+});
+describe('countLetters when it receives valid output', function() {
+  it('should return an empty object when passed an empty string', function() {
+    const input = "";
+    assert.isEmpty(countLetters(input));
+  });
+  it('should return an object', function() {
+    const input = "a";
+    assert.isObject(countLetters(input));
+  });
+  it('should not count spaces', function() {
+    const input = " ";
+    assert.equal(countLetters(input)[" "], undefined);
+  });
+  it('should ignore case, returning counts in lower case', function() {
+    const input = "Z";
+    assert.equal(countLetters(input)["Z"], undefined);
+    assert.equal(countLetters(input)["z"], 1);
+  });
+  it('should return an accurate count of all the letters in the string', function() {
+    const input = "Candy Corn";
+    let output = countLetters(input);
+    assert.equal(output["a"], 1);
+    assert.equal(output["c"], 2);
+    assert.equal(output["q"], undefined);
+  });
+});
